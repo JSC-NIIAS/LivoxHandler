40c83bea82fe31d8be139f997cae5213116d7f7d
George Chepiga <egorchepiga@gmail.com>
2020-04-06 14:22:56 +0300

    Fix. create subdir

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ if [ -d "test_lidar_cluster/build_test" ];
 then
 	rm -rf test_lidar_cluster/build_test/*
 else
-	mkdir test_lidar_cluster/build_test
+	mkdir -p test_lidar_cluster/build_test
 fi
 
 cd test_lidar_cluster/build_test
